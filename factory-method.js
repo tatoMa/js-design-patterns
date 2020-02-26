c6dcf3ff6b650d5fef1b62c183e10e817433453e
tatoMa <Jason.ma1904@gmail.com>
2020-02-26 21:34:22 +1100

    factory method ver 2

--- factory-method.js
+++ factory-method.js
@@ -22,4 +22,19 @@ class Point{
         this.x = x
         this.y = y
     }
+    static newCartesianPoint(x, y){
+        return new Point(x, y)
+    }
+    static newPolarPoint(rho, theta){
+        return new Point(
+            rho * Math.cos(theta),
+            rho * Math.sin(theta)
+        )
+    }
 }
+
+let p = Point.newCartesianPoint(4, 5)
+console.log(p);
+
+let p2 = Point.newPolarPoint(5, Math.PI/2)
+console.log(p2);
